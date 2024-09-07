9422455a424a2847c8ac739dbb6b9777de0c2f50
NugGETforFREE <hagetc1488@gmail.com>
2024-09-07 16:18:25 +0500

    Add files via upload

--- HEX2.py
+++ HEX2.py
@@ -1,14 +1,14 @@
+
 import argparse
-import struct
 
 def hex_to_dec(hex_str):
-    return int(hex_str, 16)
+    return int(hex_str, 16) if hex_str else 0
 
 def hex_to_bin(hex_str):
-    return bin(hex_to_dec(hex_str))[2:]
+    return bin(hex_to_dec(hex_str))[2:].zfill(8) if hex_str else '00000000'
 
 def hex_to_int(hex_str):
-    return int(hex_str, 16)
+    return int(hex_str, 16) if hex_str else 0
 
 def compare_files(file1_path, file2_path, output_path):
     discrepancies = []
@@ -18,13 +18,14 @@ def compare_files(file1_path, file2_path, output_path):
         while True:
             byte1 = file1.read(1)
             byte2 = file2.read(1)
-            if not byte1 and not byte2:
+
+            if not byte1 and not byte2:  # Оба файла закончились
                 break
 
+            hex1 = byte1.hex() if byte1 else ''
+            hex2 = byte2.hex() if byte2 else '00'  # Если byte2 пуст, используем '00'
+
             if byte1 != byte2:
-                hex1 = byte1.hex()
-                hex2 = byte2.hex() if byte2 else '00'
-                
                 dec1 = hex_to_dec(hex1)
                 dec2 = hex_to_dec(hex2)
                 int1 = hex_to_int(hex1)
